fda59b655a80d9460a008e0b6fa9ccbad023622d
mooujarrar <mohyiddine1997@gmail.com>
2025-02-05 23:35:23 +0100

    feat(Firmware): Initial version

---
+++ components/moisture/include/moisture.h
@@ -0,0 +1,14 @@
+#ifndef MOISTURE_H
+#define MOISTURE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/adc.h"
+#include "esp_log.h"
+
+void moisture_init(void);
+
+#endif // MOISTURE_H
---
+++ components/moisture/moisture.c
@@ -0,0 +1,30 @@
+#include "moisture.h"
+
+static const char *TAG = "Moisture sensor";
+
+static void moisture_reading(void *pvParameters) {
+    uint16_t adc_data;
+    while (1) {
+        if (ESP_OK == adc_read(&adc_data)) {
+            ESP_LOGI(TAG, "adc read: %d\r\n", adc_data);
+            // TODO: Instead of logging only the data, I should publish them in topic 'moisture' of the mqtt
+        }
+        // We take a measurement each minute
+        vTaskDelay(pdMS_TO_TICKS(6000));
+    }
+
+}
+
+void moisture_init(void) {
+    // 1. init adc
+    adc_config_t adc_config;
+
+    // Depend on menuconfig->Component config->PHY->vdd33_const value
+    // When measuring system voltage(ADC_READ_VDD_MODE), vdd33_const must be set to 255.
+    adc_config.mode = ADC_READ_TOUT_MODE;
+    adc_config.clk_div = 16; // ADC sample collection clock = 160MHz/clk_div = 10MHz
+    ESP_ERROR_CHECK(adc_init(&adc_config));
+
+    // Instanciate Task to read Moisture sensor values
+    xTaskCreate(moisture_reading, "mosture_reading_task", 4096, NULL, 5, NULL);
+}

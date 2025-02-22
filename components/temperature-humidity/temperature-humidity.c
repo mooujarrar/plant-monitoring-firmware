4bdb1f1c71b58163e5a7d3119aac479c32ad4774
mooujarrar <mohyiddine1997@gmail.com>
2025-02-22 22:54:23 +0100

    enh# sending humidity and temperature also through mqtt

--- components/temperature-humidity/temperature-humidity.c
+++ components/temperature-humidity/temperature-humidity.c
@@ -10,13 +10,18 @@ static TaskHandle_t reading_task_handle = NULL;
 static void sensor_reading_task(void *pvParameters) {
     esp_mqtt_client_handle_t mqtt_client = *(esp_mqtt_client_handle_t *)pvParameters;
     int msg_id;
-    char buffer[16];          // Buffer to hold the resulting string
+    char humidity_buffer[16];          // Buffer to hold the resulting string
+    char temp_buffer[16];          // Buffer to hold the resulting string
     setDHTgpio(GPIO_NUM_4);
     ESP_LOGI(TAG, "Starting DHT Task\n\n");
-
     while (1)
     {
-        ESP_LOGI(TAG, "=== Reading DHT ===\n");
+        // Check that mqtt_client is valid
+        if (mqtt_client == NULL) {
+            ESP_LOGE(TAG, "MQTT client handle is NULL; cannot publish.");
+            vTaskDelay(pdMS_TO_TICKS(1000)); // Delay before retry
+            continue;
+        }
         int ret = readDHT();
 
         errorHandler(ret);
@@ -24,7 +29,15 @@ static void sensor_reading_task(void *pvParameters) {
         humidity_t humidity = getHumidity();
         temperature_t temperature = getTemperature();
 
-        ESP_LOGI(TAG, "Hum: %d.%d Tmp: %d.%d \n", humidity.humidity_whole, humidity.humidity_decimal, temperature.temperature_whole, temperature.temperature_decimal);
+        // Put the read value into a char[] buffer
+        snprintf(humidity_buffer, sizeof(humidity_buffer), "%d.%d", humidity.humidity_whole, humidity.humidity_decimal);
+        snprintf(temp_buffer, sizeof(temp_buffer), "%d.%d", temperature.temperature_whole, temperature.temperature_decimal);
+
+        // Publish the moisture value to MQTT Broker
+        msg_id = esp_mqtt_client_publish(mqtt_client, "/humidity", humidity_buffer, 0, 1, 0);
+        msg_id = esp_mqtt_client_publish(mqtt_client, "/temperature", temp_buffer, 0, 1, 0);
+
+        ESP_LOGI(TAG, "Published Humidity=%d.%d, Temperature=%d.%d, msg_id=%d\r\n", humidity.humidity_whole, humidity.humidity_decimal, temperature.temperature_whole, temperature.temperature_decimal, msg_id);
 
         // -- wait at least 10 sec before reading again ------------
         // The interval of whole process must be beyond 2 seconds !!
